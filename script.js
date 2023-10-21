c3d4e3196691b8d5921077efb8c7d134baf3ed0a
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-21 19:12:25 +0200

    added the logic for lizard and spock, built  better message prompts

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // those are helpers for broken input
 let playerSelection = "It's a player trap!"
 let computerSelection = "It's a computer trap!"
-//those are for the score
 let playerScore = 0
 let computerScore = 0
 
@@ -10,7 +9,11 @@ let computerScore = 0
 let btnRock = document.querySelector('#rock');
 let btnPaper = document.querySelector('#paper');
 let btnScissors = document.querySelector('#scissors');
+let btnSpock = document.querySelector('#spock');
+let btnLizard = document.querySelector('#lizard');
 let divResults = document.querySelector('#results');
+let playerScoreValue = document.querySelector('#player-score');
+let computerScoreValue = document.querySelector('#computer-score');
 
 // DOM listener
 btnRock.addEventListener('click', () => {
@@ -28,11 +31,20 @@ btnScissors.addEventListener('click', () => {
     getComputerChoice()
     playRound(playerSelection, computerSelection)
 });
-
+btnSpock.addEventListener('click', () => {
+    playerSelection = "Spock";
+    getComputerChoice()
+    playRound(playerSelection, computerSelection)
+});
+btnLizard.addEventListener('click', () => {
+    playerSelection = "Lizard";
+    getComputerChoice()
+    playRound(playerSelection, computerSelection)
+});
 
 //get the computer choice
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3)
+    let computerChoice = Math.floor(Math.random() * 5)
     switch(computerChoice) {
         case 0:
         computerSelection = "Rock";
@@ -43,6 +55,12 @@ function getComputerChoice() {
         case 2:
         computerSelection = "Scissors";
         break;
+        case 3:
+        computerSelection = "Spock";
+        break;
+        case 4:
+        computerSelection = "Lizard";
+        break;
     }
     return computerSelection
 }
@@ -50,45 +68,67 @@ function getComputerChoice() {
 //solve the round
 function playRound(playerSel, computerSel) {
     if (playerSel == computerSel) {
-        divResults.innerHTML = divResults.innerHTML + "<br />" + `It's even: ` + playerSel + ` against ` + computerSel + "<br />" + `Player: ` + playerScore + `, Computer ` + computerScore
+        divResults.innerHTML = "It's even! " + "<br />" + playerSel + " against " + computerSel
     } else if (playerSel == "Rock") {
-        if (computerSel == "Scissors") {
+        if (computerSel == "Scissors" || computerSel == "Lizard") {
             playerScore = ++playerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You win: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You win! " + "<br />" + playerSel + " beats " + computerSel
         } else {
             computerScore = ++computerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You loose: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You loose! " + "<br />" + playerSel + " is beaten by " + computerSel
         }
     } else if (playerSel == "Paper") {
-        if (computerSel == "Rock") {
+        if (computerSel == "Rock" || computerSel == "Spock") {
             playerScore = ++playerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You win: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You win! " + "<br />" + playerSel + " beats " + computerSel
         } else {
             computerScore = ++computerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You loose: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You loose! " + "<br />" + playerSel + " is beaten by " + computerSel
         }
     } else if (playerSel == "Scissors") {
-        if (computerSel == "Paper") {
+        if (computerSel == "Paper" || computerSel == "Lizard") {
             playerScore = ++playerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You win: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You win! " + "<br />" + playerSel + " beats " + computerSel
         } else {
             computerScore = ++computerScore
-            divResults.innerHTML = divResults.innerHTML + "<br />" + "You loose: " + playerSel + " against " + computerSel + "<br />" + "Player: " + playerScore + ", Computer " + computerScore
+            divResults.innerHTML = "You loose! " + "<br />" + playerSel + " is beaten by " + computerSel
+        }
+    } else if (playerSel == "Spock") {
+        if (computerSel == "Rock" || computerSel == "Scissors") {
+            playerScore = ++playerScore
+            divResults.innerHTML = "You win! " + "<br />" + playerSel + " beats " + computerSel
+        } else {
+            computerScore = ++computerScore
+            divResults.innerHTML = "You loose! " + "<br />" + playerSel + " is beaten by " + computerSel
+        }
+    } else if (playerSel == "Lizard") {
+        if (computerSel == "Paper" || computerSel == "Spock") {
+            playerScore = ++playerScore
+            divResults.innerHTML = "You win! " + "<br />" + playerSel + " beats " + computerSel
+        } else {
+            computerScore = ++computerScore
+            divResults.innerHTML = "You loose! " + "<br />" + playerSel + " is beaten by " + computerSel
         }
     }
 
+    playerScoreValue.textContent = playerScore
+    computerScoreValue.textContent = computerScore
+
     //to add at the end of every round
     if (playerScore == 5) {
-        divResults.innerHTML = divResults.innerHTML + "<br />" + "<p style=\"color:green\">It's over, you have the high ground!</p>";
+        divResults.innerHTML = "<h1 style=\"color:green\">It's over, you have the high ground!</p>";
         btnRock.remove()
         btnPaper.remove()
         btnScissors.remove()
-
+        btnSpock.remove()
+        btnLizard.remove()
     } else if (computerScore == 5) {
-        divResults.innerHTML = divResults.innerHTML + "<br />" + "<p style=\"color:red\">It's over, and you are done</p>";
+        divResults.innerHTML = "<p style=\"color:red\">It's over, and you are done</p>";
         btnRock.remove()
         btnPaper.remove()
         btnScissors.remove()
+        btnSpock.remove()
+        btnLizard.remove()
     }
 
 }
